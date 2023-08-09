4c2bd52d38ec84bc8a932f69a976da24d1616885
anafariasilveira <139478174+anafariasilveira@users.noreply.github.com>
2023-08-09 15:56:55 -0300

    Resolução dos exercícios

--- Módulo_01/Aula_02/Exercicio_01/Exercicio_01/Program.cs
+++ Módulo_01/Aula_02/Exercicio_01/Exercicio_01/Program.cs
@@ -1,11 +1,4 @@
-﻿/* 1- Um imposto é calculado com base na seguinte tabela: 
-Até 1.200,00 isento 
-de 1.201,00 a 2.500,00 10% 
-de 2.501,00 a 5.000,00 15% 
-acima de 5.000,00 20%. 
-Implemente um programa que leia o valor base e calcule o imposto a pagar. */
-
-Console.WriteLine("Digite seu salário:");
+﻿Console.WriteLine("Digite seu salário:");
 Console.Write("R$ "); double salario = double.Parse(Console.ReadLine());
 
 if (salario <= 1200)
@@ -28,5 +21,3 @@ else if (salario > 5000)
     Console.WriteLine($"Você pagará R$ {salarioimp3} de imposto");
 }
 Console.ReadKey();
-
-//FAZER UM TRATAMENTO DE ERRO PARA QUANDO INSERIDO NÃO NÚMEROS
--- Módulo_01/Aula_02/Exercicio_04/Exercicio_04/Program.cs
+++ Módulo_01/Aula_02/Exercicio_04/Exercicio_04/Program.cs
@@ -1,10 +1,5 @@
-﻿/* 4- Escrever um programa que preencha um vetor de 100 elementos com valores inteiros aleatórios, e 
-identifique o maior e o menor número gerados, e respectivas posições. */
-
-int[] vetornum = new int[100];
+﻿int[] vetornum = new int[100];
 Random aleatorio = new Random();
-
-//Console.WriteLine(vetornum);
 
 for (int i = 0; i < vetornum.Length; i++)
 {
@@ -16,5 +11,3 @@ int menornum = vetornum.Min();
 
 Console.WriteLine($"O maior número é {maiornum} na posição {Array.IndexOf(vetornum, maiornum)}.");
 Console.WriteLine($"O menor número é {menornum} na posição {Array.IndexOf(vetornum, menornum)}.");
-
-// QUERO SABER COMO FAZ PRA IMPRIMIR OS NÚMEROS GERADOS.
