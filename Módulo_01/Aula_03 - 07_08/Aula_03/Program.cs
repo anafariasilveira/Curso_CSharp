162e0e6a30561598f9c8f47b16c1ead64df51468
anafariasilveira <139478174+anafariasilveira@users.noreply.github.com>
2023-08-09 16:27:45 -0300

    Resolução do exercício

---
+++ Módulo_01/Aula_03 - 07_08/Aula_03/Program.cs
@@ -0,0 +1,59 @@
+﻿using static Aula_03.Program;
+using static System.Console;
+using System.Globalization;
+
+namespace Aula_03
+{
+    internal class Program
+    {
+        internal class Produto
+        {
+            public string Nome;
+            public double Preco;
+            public int Estoque;
+            public void adicionaestoque (int estoque)
+            {
+                Estoque += estoque;
+            }
+            public void removeestoque(int estoque)
+            {
+                Estoque -= estoque;
+            }
+
+            public void resumo()
+            {
+                WriteLine($"O produto é {Nome}. \n Preço: R$ {Preco}. \n Quantidade em estoque {Estoque}. \n Valor total no estoque: R$ {(Preco * Estoque):N2}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Produto produto = new();
+
+            WriteLine("Informe o nome do produto:");
+            produto.Nome = ReadLine();
+
+            WriteLine("Informe o preço do produto:");
+            Write("R$ "); produto.Preco = double.Parse(ReadLine());
+          
+            WriteLine("Informe a quantidade do produto em estoque:");
+            produto.Estoque = int.Parse(ReadLine());
+
+            produto.resumo();
+            WriteLine();
+
+            WriteLine("Deseja adicionar quantos produtos?");
+            produto.adicionaestoque(int.Parse(ReadLine()));
+
+            produto.resumo();
+            WriteLine();
+
+            WriteLine("Deseja remover quantos produtos?");
+            produto.removeestoque(int.Parse(ReadLine()));
+
+            produto.resumo();
+            WriteLine();
+
+            ReadKey();                    
+        }
+    }
+}
