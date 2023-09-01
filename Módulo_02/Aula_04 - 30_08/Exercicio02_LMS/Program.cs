03d689f783dd5420c889606778311edae593d469
Ana Silveira <anapaula.carvalhofaria@gmail.com>
2023-09-01 18:58:09 -0300

    Exercício aula

---
+++ Módulo_02/Aula_04 - 30_08/Exercicio02_LMS/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exercicio02_LMS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ServicoImpressao.ImprimeValorETipo<int>(10));
+        }
+    }
+}
---
+++ Módulo_02/Aula_04 - 30_08/Exercicio02_LMS/ServicoImpressao .cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio02_LMS
+{
+    public static class ServicoImpressao
+    {
+        public static string ImprimeValorETipo<T>(T valor)
+        {
+            string tipo = typeof(T).Name;
+
+            if (tipo == "Int32")
+            {
+                tipo = "Int";
+            }
+            return $"{tipo}: {valor}";
+        }
+    }
+}
