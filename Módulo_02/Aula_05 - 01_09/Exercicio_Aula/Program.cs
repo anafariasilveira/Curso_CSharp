b9b287113e6acb78b25b284499f0792bd32cf636
anafariasilveira <139478174+anafariasilveira@users.noreply.github.com>
2023-09-02 15:57:14 -0300

    Update Program.cs

--- Módulo_02/Aula_05 - 01_09/Exercicio_Aula/Program.cs
+++ Módulo_02/Aula_05 - 01_09/Exercicio_Aula/Program.cs
@@ -1,12 +1,5 @@
 ﻿namespace Exercicio_Aula
 {
-    /* Crie um projeto Cinema
-Crie uma classe Filme com Titulo, Ano, Duracao
-Crie uma classe Sala com quantidadeCadeiras
-Crie uma classe Sessao com private Filme, private Sala e private DataHora
-No construtor da Exibicao receba a data e a sala // Injeção de dependencia via construtor
-Crie um método para receber o Filme
-    */
     internal class Program
     {
         static void Main(string[] args)
